c564542a17fd066ce0ee01bfe680deb5b828043d
Josh Birdwell <jbirdwell34@gmail.com>
2018-02-02 14:13:39 -0600

    Confirmed OU instead

--- app/server/services/stats.js
+++ app/server/services/stats.js
@@ -31,7 +31,7 @@ function calculateStats(){
     submitted: 0,
     admitted: 0,
     confirmed: 0,
-    confirmedMit: 0,
+    confirmedOU: 0,
     declined: 0,
 
     confirmedFemale: 0,
@@ -98,7 +98,7 @@ function calculateStats(){
         newStats.confirmed += user.status.confirmed ? 1 : 0;
 
         // Count confirmed that are mit
-        newStats.confirmedMit += user.status.confirmed && email === "mit.edu" ? 1 : 0;
+        newStats.confirmedOU += user.status.confirmed && email === "ou.edu" ? 1 : 0;
 
         newStats.confirmedFemale += user.status.confirmed && user.profile.gender == "F" ? 1 : 0;
         newStats.confirmedMale += user.status.confirmed && user.profile.gender == "M" ? 1 : 0;
